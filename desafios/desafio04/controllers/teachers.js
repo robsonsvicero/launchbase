65db4ec44e8360087c7b2256cdb6ac5867056119
robsonsvicero <robsonsvicero@outlook.com>
2020-10-21 22:03:04 -0300

    Correção ID no desafio 04

--- desafios/desafio04/controllers/teachers.js
+++ desafios/desafio04/controllers/teachers.js
@@ -5,128 +5,133 @@ const Intl = require('intl')
 
 exports.index = function(req, res) {
 
-  return res.render('teachers/index', { teachers: data.teachers })
+    return res.render('teachers/index', { teachers: data.teachers })
 }
 
 //CREATE
 exports.create = (req, res) => {
-  return res.render("teachers/create")
+    return res.render("teachers/create")
 }
 
 //POST
 exports.post = function(req, res) {
-  //Validando preenchimento dos dados
-  const keys = Object.keys(req.body)
+    //Validando preenchimento dos dados
+    const keys = Object.keys(req.body)
 
-  for (key of keys) {
-    if (req.body[key] == "") {
-      return res.send("Please, fill all fields!")
+    for (key of keys) {
+        if (req.body[key] == "") {
+            return res.send("Please, fill all fields!")
+        }
     }
-  }
 
-  let { avatar_url, name, birth, schooling, classes, services } = req.body
+    let { avatar_url, name, birth, schooling, classes, services } = req.body
 
-  birth = Date.parse(birth)
-  const created_at = Date.now()
-  const id = Number(data.teachers.length + 1)
-  services = services.split(",")
+    birth = Date.parse(birth)
+    const created_at = Date.now()
+    let id = 1
+    const lastTeacher = data.teachers[data.teachers.length - 1]
 
-  data.teachers.push({
-    id,
-    avatar_url,
-    name,
-    birth,
-    schooling,
-    classes,
-    services,
-    created_at
-  })
+    if (lastTeacher) {
+        id = lastTeacher.id + 1
+    }
+    services = services.split(",")
 
-  fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
-    if (err) return res.send("Write file error!")
+    data.teachers.push({
+        id,
+        avatar_url,
+        name,
+        birth,
+        schooling,
+        classes,
+        services,
+        created_at
+    })
 
-    return res.redirect("/teachers")
-  })
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+        if (err) return res.send("Write file error!")
+
+        return res.redirect("/teachers")
+    })
 }
 
 //SHOW
 exports.show = function(req, res) {
-  const { id } = req.params
+    const { id } = req.params
 
-  const foundTeacher = data.teachers.find(function(teacher) {
-    return id == teacher.id
-  })
+    const foundTeacher = data.teachers.find(function(teacher) {
+        return id == teacher.id
+    })
 
-  if (!foundTeacher) return res.send("Teacher not found!")
+    if (!foundTeacher) return res.send("Teacher not found!")
 
-  const teacher = {
-    ...foundTeacher,
-    age: age(foundTeacher.birth),
-    schooling: graduation(foundTeacher.schooling),
-    created_at: new Intl.DateTimeFormat('pt-BR').format(foundTeacher.created_at),
-  }
+    const teacher = {
+        ...foundTeacher,
+        age: age(foundTeacher.birth),
+        schooling: graduation(foundTeacher.schooling),
+        created_at: new Intl.DateTimeFormat('pt-BR').format(foundTeacher.created_at),
+    }
 
-  return res.render("teachers/show", { teacher })
+    return res.render("teachers/show", { teacher })
 }
 
 //EDIT
 exports.edit = function(req, res) {
-  const { id } = req.params
+    const { id } = req.params
 
-  const foundTeacher = data.teachers.find(function(teacher) {
-    return id == teacher.id
-  })
+    const foundTeacher = data.teachers.find(function(teacher) {
+        return id == teacher.id
+    })
 
-  if (!foundTeacher) return res.send("Teacher not found!")
-  const teacher = {
-    ...foundTeacher,
-    birth: date(foundTeacher.birth).iso
-  }
-  return res.render('teachers/edit', { teacher })
+    if (!foundTeacher) return res.send("Teacher not found!")
+    const teacher = {
+        ...foundTeacher,
+        birth: date(foundTeacher.birth).iso
+    }
+    return res.render('teachers/edit', { teacher })
 }
 
 //PUT
 exports.put = function(req, res) {
-  const { id } = req.body
-  let index = 0
+    const { id } = req.body
+    let index = 0
 
-  const foundTeacher = data.teachers.find(function(teacher, foundIndex) {
-    if (id == teacher.id) {
-      index = foundIndex
-      return true
+    const foundTeacher = data.teachers.find(function(teacher, foundIndex) {
+        if (id == teacher.id) {
+            index = foundIndex
+            return true
+        }
+    })
+
+    if (!foundTeacher) return res.send("Teacher not found!")
+
+    const teacher = {
+        ...foundTeacher,
+        ...req.body,
+        birth: Date.parse(req.body.birth),
+        id: Number(req.body.id),
+        services: req.body.services.split(",")
     }
-  })
+    data.teachers[index] = teacher
 
-  if (!foundTeacher) return res.send("Teacher not found!")
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+        if (err) return res.send("Write file error!")
 
-  const teacher = {
-    ...foundTeacher,
-    ...req.body,
-    birth: Date.parse(req.body.birth),
-    id: Number(req.body.id),
-    services: req.body.services.split(",")
-  }
-  data.teachers[index] = teacher
-
-  fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
-    if (err) return res.send("Write file error!")
-
-    return res.redirect(`/teachers/${id}`)
-  })
+        return res.redirect(`/teachers/${id}`)
+    })
 }
 
 //DELETE
 exports.delete = function(req, res) {
-  const { id } = req.body
+    const { id } = req.body
 
-  const filteredTeachers = data.teachers.filter(function(teacher) {
-    return teacher.id != id
-  })
-  data.teachers = filteredTeachers
+    const filteredTeachers = data.teachers.filter(function(teacher) {
+        return teacher.id != id
+    })
+    data.teachers = filteredTeachers
 
-  fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
-    if (err) return res.send("Write file error!")
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+        if (err) return res.send("Write file error!")
 
-    return res.redirect('/teachers')
-  })
+        return res.redirect('/teachers')
+    })
 }
